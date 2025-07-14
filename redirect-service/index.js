c9a506fc500de92c5b5fa2ad69ecfd84e0a512e8
Alexander <alexandersinyaev20@gmail.com>
2025-07-14 21:34:23 +1000

    update: add redirect-service

---
+++ redirect-service/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+const TARGET_URL = "https://bar-calc.ru";
+
+app.use((req, res) => {
+  res.redirect(301, TARGET_URL);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Redirecting all to ${TARGET_URL}`));
